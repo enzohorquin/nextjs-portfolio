7c24b90c71caf6cff6f7d923ddbe98c78128571c
Enzo Horquin <enzohorquin@gmail.com>
2020-05-07 22:28:50 -0300

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/Layout/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import { getSortedPostsData } from "../lib/posts";
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I'm a Software Engineer currently working with React and Node.</p>
+        <p>
+          <Link href="/posts/first-post">Read my Blog!</Link>
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
